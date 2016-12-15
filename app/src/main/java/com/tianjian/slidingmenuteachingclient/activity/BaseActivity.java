48d7096385b3cf9f77eceb8874843ff8012c0229
Hulk <yhmsn2008@hotmail.com>
2016-12-15 11:53:28 +0800

    基础框架搭建完成

---
+++ app/src/main/java/com/tianjian/slidingmenuteachingclient/activity/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.tianjian.slidingmenuteachingclient.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.tianjian.slidingmenuteachingclient.application.SystemApplcation;
+
+
+public class BaseActivity extends Activity {
+	protected SystemApplcation systemApplcation;
+	protected ImageButton backImg;
+	protected TextView title;//标题
+	protected Class<?> toActivity;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		systemApplcation = (SystemApplcation) getApplication();
+		systemApplcation.addActivity(this);
+	}
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+	}
+	
+	
+
+	public void setActivity(Class<?> toActivity){
+		this.toActivity =  toActivity;
+	}
+	
+	public Class<?> getActivity(){
+		return this.toActivity;
+	}
+	
+}
